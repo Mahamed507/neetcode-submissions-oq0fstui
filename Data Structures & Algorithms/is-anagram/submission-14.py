7f1b54a7ab07fe32ae2456497a5e7b076e54a3f6
Mahamed <122625156+Mahamed507@users.noreply.github.com>
2026-05-09 12:14:33 -0500

    Add: is-anagram - submission-14

---
+++ Data Structures & Algorithms/is-anagram/submission-14.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        dict1 , dict2  = {}, {}
+
+        if len(s) != len(t):
+            return False
+
+
+        for i in range(len(s)):
+            dict1[s[i]] = 1 + dict1.get(s[i] , 0)
+            dict2[t[i]] = 1 + dict2.get(t[i], 0)
+
+        return dict1 == dict2
+      
+      
+   
+
+
+        
+        
