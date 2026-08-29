70b1ad1618dae19300e1d56ca51752e338dbd1b4
Mahamed <122625156+Mahamed507@users.noreply.github.com>
2026-08-28 21:01:22 -0500

    Add: is-anagram - submission-20

---
+++ Data Structures & Algorithms/is-anagram/submission-20.py
@@ -0,0 +1,32 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+
+        map1 , map2 = {} , {}
+
+        if len(s) != len(t):
+            return False
+
+
+        for char in range(len(s)):
+            map1[s[char]] = 1 + map1.get(s[char] , 0)
+            map2[t[char]] = 1 + map2.get(t[char] , 0)
+
+        if map1.items() == map2.items():
+            return True
+
+        return False
+
+        '''
+        plan
+        1. check if the lengh of s and t. If not return False.
+        2. create two maps for s and t. 
+        3. then use a for loop , and add it into the map. make sure the values increment as well becuase we could have double letters. 
+        4. after appending insdie the two maps then i want to compare if they are the same return true otherwise return false.
+      
+
+  
+        '''
+        
+
+        
+        
